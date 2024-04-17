e12b013cf10006678e822aac0319a3c70515bb3c
Vikas Khanna <VikasK@Emtel.Org>
2024-04-17 17:03:28 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());  
+
+app.post('/webhook', (req, res) => {
+    console.log('Received webhook:', req.body);  
+
+ 
+    res.status(200).send('Webhook received!');  
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
